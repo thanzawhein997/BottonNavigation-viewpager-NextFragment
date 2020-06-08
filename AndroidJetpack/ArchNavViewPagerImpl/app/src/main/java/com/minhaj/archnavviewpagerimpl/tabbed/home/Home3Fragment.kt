cfdb9e64d35dbc579bedc71c9e115918f534dd93
Sandar <sandar@sandar>
2020-06-08 12:01:10 +0630

    first commit

---
+++ AndroidJetpack/ArchNavViewPagerImpl/app/src/main/java/com/minhaj/archnavviewpagerimpl/tabbed/home/Home3Fragment.kt
@@ -0,0 +1,68 @@
+package com.minhaj.archnavviewpagerimpl.tabbed.home
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+import android.view.KeyEvent.KEYCODE_BACK
+import android.view.KeyEvent
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.minhaj.archnavviewpagerimpl.R
+import kotlinx.android.synthetic.main.fragment_home1.*
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class Home3Fragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home3, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+        clickMeAlso.setOnClickListener {
+
+            findNavController().navigate(Home3FragmentDirections.actionHome3FragmentToHome4Fragment())
+
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+
+        if (view == null) {
+            return
+        }
+
+        view!!.isFocusableInTouchMode = true
+        view!!.requestFocus()
+        view!!.setOnKeyListener(object : View.OnKeyListener {
+            override fun onKey(v: View, keyCode: Int, event: KeyEvent): Boolean {
+
+                return if (event.getAction() === KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_BACK) {
+                    // handle back button's click listener
+                    Navigation.findNavController(v).navigateUp()
+                    true
+                } else false
+            }
+        })
+    }
+
+}
---
+++ Bottom Navigation/NavigationUiExample-/app/src/main/java/com/mationStudios/navigationuisample/MainActivity.java
@@ -0,0 +1,28 @@
+package com.mationStudios.navigationuisample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpNavigation();
+    }
+    public void setUpNavigation(){
+        bottomNavigationView =findViewById(R.id.bttm_nav);
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView,
+                navHostFragment.getNavController());
+    }
+}
